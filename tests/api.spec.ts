de872f754a7aac69cfaa94e89d0d23a9f2b71010
Mulalic97 <mustafa.mulalic@yahoo.com>
2024-01-18 14:54:28 +0100

    feat: add api test cases

---
+++ tests/api.spec.ts
@@ -0,0 +1,52 @@
+import { test } from "../utils/test";
+import { expect } from "@playwright/test";
+import { registerData } from "../utils/data";
+
+let token;
+
+test.beforeAll(async ({ request, constants }) => {
+  const response = await request.post(`${constants.webClientURL}/users`, {
+    data: {
+      firstName: registerData.name,
+      email: registerData.email,
+      password: registerData.password,
+      lastName: registerData.lastName,
+    },
+  });
+  expect(response.status()).toBe(201);
+  const res = await response.json();
+
+  token = res.token;
+});
+
+test("GET users", async ({ constants, request }) => {
+  const response = await request.get(`${constants.webClientURL}/users/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  expect(response.status()).toBe(200);
+});
+
+test("POST contacts", async ({ constants, request }) => {
+  const response = await request.post(`${constants.webClientURL}/contacts`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    data: {
+      firstName: "John",
+      lastName: "Doe",
+      birthdate: "1970-01-01",
+      email: "jdoe@fake.com",
+      phone: "8005555555",
+      street1: "1 Main St.",
+      street2: "Apartment A",
+      city: "Anytown",
+      stateProvince: "KS",
+      postalCode: "12345",
+      country: "USA",
+    },
+  });
+
+  expect(response.status()).toBe(201);
+});
